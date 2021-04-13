b50de8613659fa62de36b8563bdc7b98307ce3a3
Tyler Lovelace <tlovelac01@gmail.com>
2021-04-13 13:02:23 -0400

    CausER script update

--- code/CausalMGM/CausER_Zostavax.R
+++ code/CausalMGM/CausER_Zostavax.R
@@ -1,6 +1,6 @@
 library(devtools)
+install_github('tyler-lovelace1/rCausalMGM')
 library(huge)
-install_github('tyler-lovelace1/rCausalMGM', ref='fci-max')
 library(rCausalMGM)
 
 ## FINAL MODEL: ALL LATENT FACTORS
@@ -9,13 +9,19 @@ data <- read.delim('../../output/age_dataset/data_mat.csv', sep=',', header=T)
 Z <- read.delim('../../output/age_dataset/Z_mat.csv', sep=',', header=T)
 YZ <- cbind(Y=data$Y, Z)
 
-out.glasso <- huge(as.matrix(YZ), method="glasso" , nlambda=30, lambda.min.ratio=0.1)
+## StARS model selection for GLASSO
 
-out.select <- huge.select(out.glasso, criterion="stars", rep.num=20, stars.thresh=0.1)
+## out.glasso <- huge(as.matrix(YZ), method="glasso" , nlambda=30, lambda.min.ratio=0.1)
 
-print(paste("optimal lambda:", out.select$opt.lambda, sep=" "))
+## out.select <- huge.select(out.glasso, criterion="stars", rep.num=20, stars.thresh=0.1)
 
-ig <- adjMat2Graph(out.select$opt.icov, colnames(YZ))
+## print(paste("optimal lambda:", out.select$opt.lambda, sep=" "))
+
+## GLASSO with StARS selected lambda = 0.32
+
+final.glasso <- huge(as.matrix(YZ), method="glasso" , lambda=0.32)
+
+ig <- adjMat2Graph(final.glasso$icov[[1]], colnames(YZ))
 
 print(ig)
 saveGraph(ig, '../../output/age_dataset/YZ_full_glasso.txt')
@@ -31,13 +37,19 @@ saveGraph(g, '../../output/age_dataset/YZ_full_allLatent.txt')
 sig = c(1,2,5,7,9,15,17,19,24,36,41,42,44,45,46,49,52,53)
 YZsig <- cbind(Y=data$Y, Z[,sig])
 
-out.glasso <- huge(as.matrix(YZsig), method="glasso" , nlambda=30, lambda.min.ratio=0.1)
+## StARS model selection for GLASSO
 
-out.select <- huge.select(out.glasso, criterion="stars", rep.num=20, stars.thresh=0.1)
+## out.glasso <- huge(as.matrix(YZsig), method="glasso" , nlambda=30, lambda.min.ratio=0.1)
 
-print(paste("optimal lambda:", out.select$opt.lambda, sep=" "))
+## out.select <- huge.select(out.glasso, criterion="stars", rep.num=20, stars.thresh=0.1)
 
-ig <- adjMat2Graph(out.select$opt.icov, colnames(YZsig))
+## print(paste("optimal lambda:", out.select$opt.lambda, sep=" "))
+
+## GLASSO with StARS selected lambda = 0.32
+
+final.glasso <- huge(as.matrix(YZsig), method="glasso" , lambda=0.32)
+
+ig <- adjMat2Graph(final.glasso$icov[[1]], colnames(YZsig))
 
 print(ig)
 saveGraph(ig, '../../output/age_dataset/YZsig_full_glasso.txt')
